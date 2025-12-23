407293faf108c795fc29eb02f66f0c67e25dcc42
Neo <neo@pulumi.com>
2025-12-23 16:07:53 +0000

    preview changes - neo-preview-1766506073223

---
+++ index.ts
@@ -0,0 +1,204 @@
+import * as pulumi from "@pulumi/pulumi";
+import * as azure from "@pulumi/azure-native";
+import * as cosmosdb from "@pulumi/azure-native/cosmosdb";
+import * as insights from "@pulumi/azure-native/applicationinsights";
+import * as web from "@pulumi/azure-native/web";
+
+// Configuration values from ARM parameters
+const config = new pulumi.Config();
+const appName = "podinfo-webapp-28525";
+const location = "eastus";
+const resourceGroupName = "podinfo-webapp-rg";
+const cosmosDbAccountName = "podinfo-cosmosdb-28525";
+const cosmosDbDatabaseName = "appdb";
+const cosmosDbContainerName = "items";
+const containerImage = "stefanprodan/podinfo:latest";
+const containerPort = 9898;
+const appServicePlanSku = "P1V2";
+const appServicePlanCapacity = 2;
+
+// Derived names (matching ARM template variables)
+const appServicePlanName = `${appName}-plan`;
+const applicationInsightsName = `${appName}-insights`;
+
+// Reference existing resource group
+const resourceGroup = azure.resources.ResourceGroup.get("resourceGroup", 
+    `/subscriptions/32b9cb2e-69be-4040-80a6-02cd6b2cc5ec/resourceGroups/${resourceGroupName}`);
+
+// Cosmos DB Account
+const cosmosAccount = new cosmosdb.DatabaseAccount("cosmosAccount", {
+    accountName: cosmosDbAccountName,
+    resourceGroupName: resourceGroupName,
+    location: location,
+    kind: "GlobalDocumentDB",
+    databaseAccountOfferType: "Standard",
+    consistencyPolicy: {
+        defaultConsistencyLevel: "Session",
+    },
+    locations: [{
+        locationName: location,
+        failoverPriority: 0,
+        isZoneRedundant: false,
+    }],
+    capabilities: [{
+        name: "EnableServerless",
+    }],
+    enableAutomaticFailover: false,
+    enableMultipleWriteLocations: false,
+}, {
+    import: "/subscriptions/32b9cb2e-69be-4040-80a6-02cd6b2cc5ec/resourceGroups/podinfo-webapp-rg/providers/Microsoft.DocumentDB/databaseAccounts/podinfo-cosmosdb-28525",
+});
+
+// Cosmos DB SQL Database
+const cosmosDatabase = new cosmosdb.SqlResourceSqlDatabase("cosmosDatabase", {
+    accountName: cosmosDbAccountName,
+    resourceGroupName: resourceGroupName,
+    databaseName: cosmosDbDatabaseName,
+    resource: {
+        id: cosmosDbDatabaseName,
+    },
+}, {
+    dependsOn: [cosmosAccount],
+    import: "/subscriptions/32b9cb2e-69be-4040-80a6-02cd6b2cc5ec/resourceGroups/podinfo-webapp-rg/providers/Microsoft.DocumentDB/databaseAccounts/podinfo-cosmosdb-28525/sqlDatabases/appdb",
+});
+
+// Cosmos DB SQL Container
+const cosmosContainer = new cosmosdb.SqlResourceSqlContainer("cosmosContainer", {
+    accountName: cosmosDbAccountName,
+    resourceGroupName: resourceGroupName,
+    databaseName: cosmosDbDatabaseName,
+    containerName: cosmosDbContainerName,
+    resource: {
+        id: cosmosDbContainerName,
+        partitionKey: {
+            paths: ["/id"],
+            kind: "Hash",
+        },
+        indexingPolicy: {
+            indexingMode: "consistent",
+            automatic: true,
+            includedPaths: [{
+                path: "/*",
+            }],
+            excludedPaths: [{
+                path: "/\"_etag\"/?",
+            }],
+        },
+    },
+}, {
+    dependsOn: [cosmosDatabase],
+    import: "/subscriptions/32b9cb2e-69be-4040-80a6-02cd6b2cc5ec/resourceGroups/podinfo-webapp-rg/providers/Microsoft.DocumentDB/databaseAccounts/podinfo-cosmosdb-28525/sqlDatabases/appdb/containers/items",
+});
+
+// Application Insights
+const appInsights = new insights.Component("appInsights", {
+    resourceName: applicationInsightsName,
+    resourceGroupName: resourceGroupName,
+    location: location,
+    kind: "web",
+    applicationType: "web",
+    retentionInDays: 90,
+    publicNetworkAccessForIngestion: "Enabled",
+    publicNetworkAccessForQuery: "Enabled",
+}, {
+    import: "/subscriptions/32b9cb2e-69be-4040-80a6-02cd6b2cc5ec/resourceGroups/podinfo-webapp-rg/providers/Microsoft.Insights/components/podinfo-webapp-28525-insights",
+});
+
+// App Service Plan
+const appServicePlan = new web.AppServicePlan("appServicePlan", {
+    name: appServicePlanName,
+    resourceGroupName: resourceGroupName,
+    location: location,
+    sku: {
+        name: appServicePlanSku,
+        capacity: appServicePlanCapacity,
+    },
+    kind: "linux",
+    reserved: true,
+}, {
+    import: "/subscriptions/32b9cb2e-69be-4040-80a6-02cd6b2cc5ec/resourceGroups/podinfo-webapp-rg/providers/Microsoft.Web/serverFarms/podinfo-webapp-28525-plan",
+});
+
+// Web App
+const webApp = new web.WebApp("webApp", {
+    name: appName,
+    resourceGroupName: resourceGroupName,
+    location: location,
+    serverFarmId: appServicePlan.id,
+    kind: "app,linux,container",
+    httpsOnly: true,
+    siteConfig: {
+        linuxFxVersion: `DOCKER|${containerImage}`,
+        alwaysOn: true,
+        http20Enabled: true,
+        minTlsVersion: "1.2",
+        ftpsState: "Disabled",
+        healthCheckPath: "/healthz",
+        autoHealEnabled: true,
+        autoHealRules: {
+            triggers: {
+                statusCodes: [{
+                    status: 500,
+                    subStatus: 0,
+                    count: 10,
+                    timeInterval: "00:05:00",
+                }],
+            },
+            actions: {
+                actionType: "Recycle",
+            },
+        },
+        appSettings: [
+            {
+                name: "WEBSITES_ENABLE_APP_SERVICE_STORAGE",
+                value: "false",
+            },
+            {
+                name: "DOCKER_REGISTRY_SERVER_URL",
+                value: "https://index.docker.io",
+            },
+            {
+                name: "WEBSITES_PORT",
+                value: containerPort.toString(),
+            },
+            {
+                name: "APPINSIGHTS_INSTRUMENTATIONKEY",
+                value: appInsights.instrumentationKey,
+            },
+            {
+                name: "APPLICATIONINSIGHTS_CONNECTION_STRING",
+                value: appInsights.connectionString,
+            },
+            {
+                name: "COSMOS_DB_ENDPOINT",
+                value: cosmosAccount.documentEndpoint,
+            },
+            {
+                name: "COSMOS_DB_KEY",
+                value: pulumi.secret(cosmosAccount.id.apply((id: string) => 
+                    cosmosdb.listDatabaseAccountKeys({
+                        accountName: cosmosDbAccountName,
+                        resourceGroupName: resourceGroupName,
+                    }).then((keys: any) => keys.primaryMasterKey!)
+                )),
+            },
+            {
+                name: "COSMOS_DB_DATABASE",
+                value: cosmosDbDatabaseName,
+            },
+            {
+                name: "COSMOS_DB_CONTAINER",
+                value: cosmosDbContainerName,
+            },
+        ],
+    },
+}, {
+    dependsOn: [appServicePlan, appInsights, cosmosAccount],
+    import: "/subscriptions/32b9cb2e-69be-4040-80a6-02cd6b2cc5ec/resourceGroups/podinfo-webapp-rg/providers/Microsoft.Web/sites/podinfo-webapp-28525",
+});
+
+// Outputs
+export const webAppUrl = pulumi.interpolate`https://${webApp.defaultHostName}`;
+export const webAppName = webApp.name;
+export const cosmosDbEndpoint = cosmosAccount.documentEndpoint;
+export const applicationInsightsKey = appInsights.instrumentationKey;
