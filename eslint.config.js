581d0e50b95076da1437a2534f8dee92f05d1ccc
Neo <neo@pulumi.com>
2025-12-30 22:57:48 +0000

    preview changes - neo-preview-1767135468657

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+
+export default [
+    js.configs.recommended,
+    {
+        files: ['**/*.ts'],
+        languageOptions: {
+            parser: tsparser,
+            parserOptions: {
+                ecmaVersion: 2020,
+                sourceType: 'module',
+            },
+        },
+        plugins: {
+            '@typescript-eslint': tseslint,
+        },
+        rules: {
+            '@typescript-eslint/no-unused-vars': 'error',
+            '@typescript-eslint/explicit-function-return-type': 'off',
+            '@typescript-eslint/no-explicit-any': 'warn',
+        },
+    },
+];
